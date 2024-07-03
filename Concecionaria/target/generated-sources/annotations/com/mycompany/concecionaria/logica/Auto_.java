64e2283480de88a7fccaeeddf32b0626f3af36c3
Damian674 <st_demian@hotmail.com>
2024-07-03 16:57:54 -0300

    cambios en proyecto agenda electronica y concecionaria

---
+++ Concecionaria/target/generated-sources/annotations/com/mycompany/concecionaria/logica/Auto_.java
@@ -0,0 +1,19 @@
+package com.mycompany.concecionaria.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-27T12:27:23", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Auto.class)
+public class Auto_ { 
+
+    public static volatile SingularAttribute<Auto, String> marca;
+    public static volatile SingularAttribute<Auto, String> motor;
+    public static volatile SingularAttribute<Auto, String> color;
+    public static volatile SingularAttribute<Auto, Integer> cantidad_puertas;
+    public static volatile SingularAttribute<Auto, Integer> id;
+    public static volatile SingularAttribute<Auto, String> modelo;
+    public static volatile SingularAttribute<Auto, String> patente;
+
+}
